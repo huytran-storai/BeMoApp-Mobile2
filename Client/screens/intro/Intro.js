539e404f9070950a0ee5349b7d4ce4fb6af4fd1f
TranSyHuy <syhuy01072002@gmail.com>
2023-06-30 12:37:17 +0700

    [GM003] [Implement] Tutorial and Intro Pages

---
+++ Client/screens/intro/Intro.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { Image, Dimensions, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const { width, height } = Dimensions.get('window');
+
+const Intro = ({ navigation }) => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('Tutorial');
+    }, 3000);
+    return () => clearTimeout(timer); 
+  }, [navigation]);
+  return (
+    <SafeAreaView style={{ width, height, backgroundColor:'#429F9E', justifyContent: 'center', alignItems: 'center' }}>
+       <Image source={require('../../assets/images/logo2.png')} style={{ width: width * 0.7, height: height * 0.3, resizeMode: 'contain', marginLeft: -15 }} />
+    </SafeAreaView>
+  );
+};
+
+export default Intro;
---
+++ Client/screens/tutorial/TutorialScreen.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Text, Image, Dimensions, View, TouchableOpacity } from 'react-native'
+import Swiper from 'react-native-swiper'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const TutorialScreen = ({ navigation }) => {
+    const goToLogIn = () => {
+        navigation.navigate('Login');
+    }
+    return (
+        <Swiper loop={false}>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 justify-center mt-10 mb-1">
+                    <Image source={require('../../assets/images/intro1.png')} className="mt-0" style={{ width: 200, height: 600, objectFit: 'contain', alignSelf: 'center' }} />
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>Chúng tôi cung cấp dịch vụ giao đồ ăn/ thức uống.</Text>
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>Chúng tôi chắc chắn bạn sẽ hài lòng với dịch vụ của chúng tôi</Text>
+                </View>
+                <View className="flex-1 justify-center">
+                    <TouchableOpacity onPress={goToLogIn} className="flex-1 justify-center" >
+                        <Text style={{ color: '#429F9E', fontWeight: 700, textAlign: 'center' }}>{"Đăng nhập ngay!"}</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 justify-center mt-10 mb-1">
+                    <Image source={require('../../assets/images/loginandsignup.png')} className="mt-0" style={{ width: 300, height: 600, objectFit: 'contain', alignSelf: 'center' }} />
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>Đầu tiên bạn phải tạo một tài khoản</Text>
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>để trở thành thành viên của chúng tôi</Text>
+                </View>
+                <View className="flex-1 justify-center">
+                    <TouchableOpacity className="flex-1 justify-center" >
+                    <Text style={{ color: '#429F9E', fontWeight: 700, textAlign: 'center' }}>{"Đăng nhập ngay!"}</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 justify-center mt-10 mb-1">
+                    <Image source={require('../../assets/images/intro3.png')} className="mt-0" style={{ width: 300, height: 600, objectFit: 'contain', alignSelf: 'center' }} />
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>Sau khi tạo tài khoản, bạn có thể chọn món ăn</Text>
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>bạn muốn thêm vào giỏ hàng của bạn và tiến hành chấp nhận đặt món</Text>
+                </View>
+                <View className="flex-1 justify-center">
+                    <TouchableOpacity  className="flex-1 justify-center" >
+                    <Text style={{ color: '#429F9E', fontWeight: 700, textAlign: 'center' }}>{"Đăng nhập ngay!"}</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+            <SafeAreaView className="bg-white w-full h-full justify-center items-center">
+                <View className="flex-1 justify-center mt-10 mb-1">
+                    <Image source={require('../../assets/images/intro4.png')} className="mt-0" style={{ width: 300, height: 600, objectFit: 'contain', alignSelf: 'center' }} />
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>Sau khi đặt món thành công</Text>
+                    <Text className="text-black left-0 text-center text-sm font-medium leading-4" style={{ textAlign: 'center' }}>món ăn sẽ đến với bạn trong thời gian dự kiến!</Text>
+                </View>
+                <View className="flex-1 justify-center">
+                    <TouchableOpacity className="flex-1 justify-center" >
+                    <Text style={{ color: '#429F9E', fontWeight: 700, textAlign: 'center' }}>{"Đăng nhập ngay!"}</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        </Swiper>
+    )
+}
+export default TutorialScreen;
