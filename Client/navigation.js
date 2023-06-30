e177abc0e154a2b41e64375aaac06961fe69a709
TranSyHuy <syhuy01072002@gmail.com>
2023-06-30 12:34:18 +0700

    [BM-00] [SetUp] Import and Install Navigation and some packge

---
+++ Client/navigation.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Intro from './screens/intro/Intro';
+import TutorialScreen from './screens/tutorial/TutorialScreen';
+const Stack = createNativeStackNavigator();
+
+import React from 'react';
+export default function Navigation() {
+    return(
+        <NavigationContainer >
+                <Stack.Navigator initialRouteName="Intro" screenOptions={{
+                    headerShown: false
+                }}>
+                    <Stack.Screen name="Intro" component={Intro} />
+                    <Stack.Screen name="Tutorial" component={TutorialScreen} />
+                    {/* <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name='SignUp' component={SignUpScreen} /> */}
+                </Stack.Navigator>
+            </NavigationContainer>
+    )
+}
